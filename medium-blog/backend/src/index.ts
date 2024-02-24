1b12a3b8e9b16095c442e35ac886f675efd43a50
Prem <prempravinsapkale@gmail.com>
2024-02-24 20:30:40 +0530

    signup and signin route added

--- medium-blog/backend/src/index.ts
+++ medium-blog/backend/src/index.ts
@@ -1,23 +1,64 @@
 import { Hono } from 'hono';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { sign } from 'hono/jwt';
 
-const prisma = new PrismaClient({
-   datasourseUrl: env.DATABASE_URL,
-}).$extends(withAccelerate());
+// ! try to avoid as many global variables as you can for serverless thingies
 
-const app = new Hono();
+// To get the right types on 'c.env', when initializing the Hono app, pass the types of env as a generic
+const app = new Hono<{
+   Bindings: {
+      DATABASE_URL: string;
+      JWT_SECRET: string;
+   };
+}>();
 
-app.get('/', (c) => {
+app.get('/', async (c) => {
    return c.text('Hello Hono!');
 });
 
-app.post('/api/v1/signup', (c) => {
-   return c.text('signup route');
+app.post('/api/v1/signup', async (c) => {
+   // from 'wrangler.toml'
+   const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+   }).$extends(withAccelerate());
+
+   const body = await c.req.json();
+   try {
+      const user = await prisma.user.create({
+         data: {
+            email: body.email,
+            password: body.password,
+         },
+      });
+
+      const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+      return c.json({ jwt });
+   } catch (e) {
+      c.status(403);
+      return c.json({ error: 'error while signing up' });
+   }
 });
 
-app.post('/api/v1/signin', (c) => {
-   return c.text('signin route');
+app.post('/api/v1/signin', async (c) => {
+   const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+   }).$extends(withAccelerate());
+
+   const body = await c.req.json();
+   const user = await prisma.user.findUnique({
+      where: {
+         email: body.email,
+      },
+   });
+
+   if (!user) {
+      c.status(403);
+      return c.json({ error: 'user not found' });
+   }
+
+   const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+   return c.json({ jwt });
 });
 
 app.get('/api/v1/blog/:id', (c) => {
