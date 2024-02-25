3ee98d8f70c47f741adfd2c9cbbac266b2118e94
Prem <prempravinsapkale@gmail.com>
2024-02-25 19:39:07 +0530

    zod types addedd on common

--- medium-blog/backend/src/index.ts
+++ medium-blog/backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
-import { sign } from 'hono/jwt';
+import { sign, verify } from 'hono/jwt';
 
 // ! try to avoid as many global variables as you can for serverless thingies
 
@@ -11,7 +11,26 @@ const app = new Hono<{
       DATABASE_URL: string;
       JWT_SECRET: string;
    };
+   Variables: {
+      userId: string;
+   };
 }>();
+
+app.use('/api/v1/blog/*', async (c, next) => {
+   const jwt = c.req.header('Authorization');
+   if (!jwt) {
+      c.status(401);
+      return c.json({ error: 'unauthorized' });
+   }
+   const token = jwt.split(' ')[1];
+   const payload = await verify(token, c.env.JWT_SECRET);
+   if (!payload) {
+      c.status(401);
+      return c.json({ error: 'unauthorized' });
+   }
+   c.set('userId', payload.id);
+   await next();
+});
 
 app.get('/', async (c) => {
    return c.text('Hello Hono!');
@@ -68,12 +87,43 @@ app.get('/api/v1/blog/:id', (c) => {
    return c.text('get blog route');
 });
 
-app.post('/api/v1/blog', (c) => {
-   return c.text('post blog route');
+app.post('/api/v1/blog', async (c) => {
+   const userId = c.get('userId');
+   const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+   }).$extends(withAccelerate());
+
+   const body = await c.req.json();
+   const post = await prisma.post.create({
+      data: {
+         title: body.title,
+         content: body.content,
+         authorId: userId,
+      },
+   });
+   return c.json({
+      id: post.id,
+   });
 });
 
-app.put('/api/v1/blog', (c) => {
-   return c.text('update blog route');
+app.put('/api/v1/blog', async (c) => {
+   const userId = c.get('userId');
+   const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+   }).$extends(withAccelerate());
+
+   const body = await c.req.json();
+   const post = await prisma.post.update({
+      where: {
+         id: body.id,
+         authorId: userId,
+      },
+      data: {
+         title: body.title,
+         content: body.content,
+      },
+   });
+   return c.json({ post });
 });
 
 export default app;
